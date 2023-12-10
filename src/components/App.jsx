58647838d1111dc802e64796e91788cc5f86b893
tmitsay <tmitsay@gmail.com>
2023-12-10 17:46:46 +0200

    up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,13 @@ import { Route, Routes } from 'react-router-dom';
 import { Layout } from './Layout/layout';
 import { lazy } from 'react';
 
-const HomePage = lazy(() => import('../pages/HomePage'));
-const MoviesPage = lazy(() => import('../pages/MoviesPage'));
-const MoviesDetails = lazy(() => import('../pages/MoviesDetailsPage'));
-const Cast = lazy(() => import('../components/Cast'));
-const Reviews = lazy(() => import('../components/Reviews'));
+const HomePage = lazy(() => import('../pages/HomePage/HomePage'));
+const MoviesPage = lazy(() => import('../pages/MoviesPage/MoviesPage'));
+const MoviesDetails = lazy(() =>
+  import('../pages/MoviesDetailsPage/moviesDetails')
+);
+const Cast = lazy(() => import('../components/Cast/cast'));
+const Reviews = lazy(() => import('../components/Reviews/reviews'));
 
 export const App = () => {
   return (
